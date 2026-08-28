a0ae170f1a7257f4999398e99d7f342b776bf6a0
gracemwai <gracejmwai@gmail.com>
2026-08-28 13:51:33 +0300

    Merge pull request #2 from akirachix/feature/technical-documentation
    Feature/technical documentation

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,47 @@
+export default {
+  title: 'PROBE',             
+  description: 'Connect suitable batteries to their next useful life.',         
+
+  themeConfig: {
+    nav: [],
+
+    sidebar: [
+      {
+        text: 'Documentation',
+        items: [
+          { text: 'Overview', link: '/overview' },
+          { text: 'Architecture', link: '/architecture' },
+           {
+            text: 'Backend',
+            collapsed: true,
+            items: [
+              { text: 'Overview', link: '/backend/overview' },
+              { text: 'Setup & Configuration', link: '/backend/setup' },
+              { text: 'Authentication', link: '/backend/authentication' },
+              { text: 'API Reference', link: '/backend/api-reference' },
+              { text: 'Database', link: '/backend/database' },
+              { text: 'Testing', link: '/backend/testing' },
+              { text: 'Deployment', link: '/backend/deployment' }
+            ]
+          },
+          { text: 'Frontend Web', link: '/frontend-web' },
+          { text: 'Hardware and IoT', link: '/hardware' },
+          { text: 'Frontend Mobile', link: '/mobile' },
+          { text: 'Security', link: '/security' },
+          { text: 'Integration', link: '/integration' },
+          { text: 'Code Standards', link: '/code-standards' },
+          { text: 'Deployment', link: '/deployment' },
+          { text: 'QA', link: '/qa' },
+          
+
+
+
+         
+          { text: 'Glossary', link: '/glossary' },
+        ]
+      }
+    ]
+
+    
+  }
+}
